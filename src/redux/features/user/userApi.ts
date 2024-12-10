26169b1a8d18bf7b021d53f17347a7ccab2be1d9
RobinTheRedLight <robinmitra789@gmail.com>
2024-12-10 16:38:42 +0600

    user-api created

---
+++ src/redux/features/user/userApi.ts
@@ -0,0 +1,36 @@
+import { baseApi } from "../../api/baseApi";
+
+export const flightsApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getUsers: builder.query({
+      query: () => ({
+        url: "/users",
+        method: "GET",
+      }),
+      providesTags: ["Users"],
+    }),
+
+    getSingleUser: builder.query({
+      query: (id) => ({
+        url: `/users/${id}`,
+        method: "GET",
+      }),
+      providesTags: ["User"],
+    }),
+
+    updateProfile: builder.mutation({
+      query: ({ profileData, id }) => ({
+        url: `/users/${id}`,
+        method: "PUT",
+        body: profileData,
+      }),
+      invalidatesTags: ["User", "Users"],
+    }),
+  }),
+});
+
+export const {
+  useGetUsersQuery,
+  useGetSingleUserQuery,
+  useUpdateProfileMutation,
+} = flightsApi;
